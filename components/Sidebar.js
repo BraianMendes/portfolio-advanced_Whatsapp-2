2edd753aec71d977e2eb7dd1f77dcf01e9a9b3d0
Braian Corrêa <00.braian.dev@gmail.com>
2021-04-23 16:47:11 -0300

    [Component] Create Sidebar

---
+++ components/Sidebar.js
@@ -0,0 +1,80 @@
+import { Avatar, IconButton, Button } from "@material-ui/core";
+import styled from "styled-components";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchIcon from "@material-ui/icons/Search";
+
+function Sidebar() {
+  return (
+    <Container>
+      <Header>
+        <UserAvatar />
+
+        <IconsContainer>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </IconsContainer>
+      </Header>
+
+      <Search>
+        <SearchIcon />
+        <SearchInput placeholder="Search in chats" />
+      </Search>
+
+      <SidebarButton>Start a new chat</SidebarButton>
+    </Container>
+  );
+}
+
+export default Sidebar;
+
+const Container = styled.div``;
+
+const Header = styled.div`
+  display: flex;
+  position: sticky;
+  z-index: 1;
+  top: 0;
+  heigth: 80px;
+  padding: 15px;
+  justify-content: space-between;
+  align-items: center;
+  background-color: white;
+  border-bottom: 1px solid whitesmoke;
+`;
+
+const UserAvatar = styled(Avatar)`
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.8;
+  }
+`;
+
+const IconsContainer = styled.div``;
+
+const Search = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 5px;
+  border-radius: 2px;
+`;
+
+const SearchInput = styled.input`
+  outline-width: 0;
+  border: none;
+  flex: 1;
+`;
+
+const SidebarButton = styled(Button)`
+  width: 100%;
+
+  &&& {
+    border-top: 1px solid whitesmoke;
+    border-bottom: 1px solid whitesmoke;
+  }
+`;
---
+++ pages/chat.js
@@ -0,0 +1,9 @@
+import Sidebar from '../components/Sidebar'
+
+export default function chat() {
+    return (
+        <div>
+            <Sidebar />
+        </div>
+    )
+}
