d73d8d70a579031b0dbb0f0cb38130dde4eb1925
Braian Corrêa <00.braian.dev@gmail.com>
2021-04-25 00:29:09 -0300

    [Feat] Chat Logic

--- components/Chat.js
+++ components/Chat.js
@@ -1,19 +1,27 @@
+import { useRouter } from "next/router";
 import { Avatar } from "@material-ui/core";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollection } from "react-firebase-hooks/firestore";
 import styled from "styled-components";
-import { auth } from "../firebase";
+import { auth, db } from "../firebase";
 import getRecipientEmail from "../utils/getRecipientEmail";
 
 function Chat({ id, users }) {
+  const router = useRouter();
   const [user] = useAuthState(auth);
   const [recipientSnapshot] = useCollection(
     db.collection("users").where("email", "==", getRecipientEmail(users, user))
   );
+
+  const enterChat = () => {
+    router.push(`/chat/${id}`);
+  };
+
   const recipient = recipientSnapshot?.docs?.[0]?.data();
   const recipientEmail = getRecipientEmail(users, user);
 
   return (
-    <Container>
+    <Container onClick={enterChat}>
       {recipient ? (
         <UserAvatar src={recipient?.photoURL} />
       ) : (
