0591ed1834bd48ceadbbe2a038021aa7a106178b
Braian Corrêa <00.braian.dev@gmail.com>
2021-04-25 00:52:59 -0300

    [Feat] Chat Screen

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -1,14 +1,97 @@
-import React from 'react'
+import React from "react";
+import { useRouter } from "next/router";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { auth } from "../firebase";
 import styled from "styled-components";
+import { Avatar, IconButton } from "@material-ui/core";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
 
-const ChatScreen = () => {
-    return (
-        <Container>
-            <h1>This is a chat</h1>
-        </Container>
-    )
-}
+const ChatScreen = ({ chat, messages }) => {
+  const [user] = useAuthState(auth);
+  const router = useRouter();
+  const [messagesSnapshot] = useCollection(
+    db
+      .collection("chats")
+      .doc(roter.query.id)
+      .collection("messages")
+      .orderBy("timestamp", "asc")
+  );
 
-export default ChatScreen
+  const showMessages = () => {
+    if (messagesSnapshot) {
+      return messagesSnapshot.docs.map((message) => (
+        <Message
+          key={message.id}
+          user={message.data().user}
+          message={{
+            ...message.data(),
+            timestamp: message.data().timestamp?.toDate().getTime(),
+          }}
+        />
+      ));
+    }
+  };
 
-const Container = styled.div``
+  return (
+    <Container>
+      <Header>
+        <Avatar />
+        <HeaderInformation>
+          <h3>Rec Email</h3>
+          <p>Last seen...</p>
+        </HeaderInformation>
+        <HeaderIcons>
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </HeaderIcons>
+      </Header>
+
+      <MessageContainer>
+        {/* show messages */}
+        <EndOfMessage />
+      </MessageContainer>
+    </Container>
+  );
+};
+
+export default ChatScreen;
+
+const Container = styled.div``;
+
+const Header = styled.div`
+  position: sticky;
+  background-color: white;
+  z-index: 100;
+  top: 0;
+  display: flex;
+  padding: 11px;
+  height: 80px;
+  align-items: center;
+  border-bottom: 1px solid whitesmoke;
+`;
+
+const HeaderInformation = styled.div`
+  margin-left: 15px;
+  flex: 1;
+
+  > h3 {
+    margin-bottom: 3px;
+  }
+
+  > p {
+    font-size: 14px;
+    color: gray;
+  }
+`;
+
+const HeaderIcons = styled.div``;
+
+const EndOfMessage = styled.div``;
+
+const MessageContainer = styled.div``;
